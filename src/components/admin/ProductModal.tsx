85550dde1dcd9f2fad810750032c0c60e63795ba
theussoares <matheusfs774@gmail.com>
2025-10-15 12:22:10 -0400

    subindo o build para master

--- src/components/admin/ProductModal.tsx
+++ src/components/admin/ProductModal.tsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from 'react';
 import { Loader2 } from 'lucide-react';
 import {
-  Dialog,
+  Dialog, 
   DialogContent,
   DialogDescription,
   DialogHeader,
