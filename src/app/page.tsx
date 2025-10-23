0ab52c1f883d29610be4b492ff95d47e69627e9a
theussoares <matheusfs774@gmail.com>
2025-10-23 11:47:40 -0400

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -71,7 +71,7 @@ export default function AdminPartnerPage() {
   //     </div>
   //   );
   // }
-
+ 
   return (
     <Suspense fallback={<div className="min-h-screen flex items-center justify-center">Carregando...</div>}>
       <AdminApp />
